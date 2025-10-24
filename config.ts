4eab147eb78bf805d9db8a28737ebdc6039cdd92
Maneesh-Test <84768096+Maneesh-Test@users.noreply.github.com>
2025-10-24 17:46:17 +0200

    Updated config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,2 @@
-// IMPORTANT: Replace these placeholders with your actual Supabase project URL and anon key.
-// 1. Go to your Supabase project dashboard.
-// 2. Navigate to Project Settings > API.
-// 3. Find your Project URL and anon Public key.
-export const SUPABASE_URL = 'https://vmhoholpeieigwcfpffb.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaG9ob2xwZWllaWd3Y2ZwZmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MTU0MTgsImV4cCI6MjA3NTk5MTQxOH0.6WuoycX7nCSErM3Zfi1Uf3EMrYaz2MEKfnSguwi9Nb8';
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || '';
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
